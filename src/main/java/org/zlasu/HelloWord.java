90331ae0e3ae99c8bca98056b59a3395622cda46
Janek <zlaasu@gmail.com>
2019-10-08 23:50:58 +0200

    Add uat profile

---
+++ src/main/java/org/zlasu/HelloWord.java
@@ -0,0 +1,9 @@
+package org.zlasu;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("###############################");
+        System.out.println("#####  WELKOMEN BITE!!!  ######");
+        System.out.println("###############################");
+    }
+}
